6d0653b0bcb80c1105ae4a61671efc4cab788b8d
Farhan <farhan.hammad@gmail.com>
2021-01-09 01:01:59 +0500

    Initial Commit

--- inactive.js
+++ inactive.js
@@ -1,24 +1,30 @@
 const user = {
-    name: "Qasim Asad",
-    age: "25",
-    city: "san francisco",
-    lastActivity: new Date().getTime()
-}
+  name: "Qasim Asad",
+  age: "25",
+  city: "san francisco",
+  lastActivity: new Date().getTime(),
+};
 
 const inactiveFirstStage = () => {
-    console.log("User has been inactive for the past 1-2 minutes");
-}
+  console.log("User has been inactive for the past 1-2 minutes");
+};
 
 const inactiveSecondStage = () => {
-    console.log("User has been inactive for the past 3-4 minutes");
-}
+  console.log("User has been inactive for the past 3-4 minutes");
+};
 
 const inactiveThirdStage = () => {
-    console.log("User has been inactive for the past 5-6 minutes");
-}
-
+  console.log("User has been inactive for the past 5-6 minutes");
+};
 
 // this is the main method that acts like a worker and runs every 1 minute
-setInterval(function(){
-    // please write your code here
-}, 1000);
+setInterval(function () {
+  // please write your code here
+  let time2 = Math.floor(new Date().getTime());
+  let minuteTime = (time2 - user.lastActivity) / 1000 / 60;
+
+  if (minuteTime <= 2) inactiveFirstStage();
+  else if (minuteTime <= 4) inactiveSecondStage();
+  else if (minuteTime <= 6) inactiveThirdStage();
+  else if (minuteTime > 6) user.lastActivity = new Date().getTime();
+}, 60000);
